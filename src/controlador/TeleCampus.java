986605161c01458dd53da2843f5a7b0493d46d62
Wikicamus <camilo20030213@gmail.com>
2023-11-20 17:35:20 -0500

    segundo commit

--- src/controlador/TeleCampus.java
+++ src/controlador/TeleCampus.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
  */
-package telecampus;
+package controlador;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -18,7 +18,7 @@ public class TeleCampus extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/vista/VistaRegister.fxml"));
         
         Scene scene = new Scene(root);
         
