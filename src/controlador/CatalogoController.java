4cffb6bedcd008a77a8ae1a6b5571eb07d1f7b18
Wikicamus <camilo20030213@gmail.com>
2023-12-07 01:41:18 -0500

    v12.2

--- src/controlador/CatalogoController.java
+++ src/controlador/CatalogoController.java
@@ -8,13 +8,17 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -25,6 +29,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * FXML Controller class
@@ -71,6 +76,14 @@ public class CatalogoController implements Initializable {
     private AnchorPane contCar;
     @FXML
     private VBox tblProdCar;
+    @FXML
+    private Label lblPrecio1;
+    @FXML
+    private Label lblPrecio2;
+    @FXML
+    private Label lblPrecio3;
+    @FXML
+    private ImageView btnCar;
 
     /**
      * Initializes the controller class.
@@ -96,24 +109,22 @@ public class CatalogoController implements Initializable {
     }
 
     @FXML
-    private void irCarrito(ActionEvent event) {
-
-    }
-
-    @FXML
-    private void irLista(ActionEvent event) {
-        contTblFav.setVisible(true);
-
-    }
-
-    @FXML
     private void IrPago(ActionEvent event) {
-    }
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista/OpcionesPago.fxml"));
+            Parent root = loader.load();
 
-    @FXML
-    private void IrLis(ActionEvent event) {
-        contTblFav.setVisible(true);
+            Stage vtnRegister = new Stage();
+            vtnRegister.setTitle("REGISTRO");
+            vtnRegister.setScene(new Scene(root));
 
+            // Obtén el controlador de la segunda ventana si es necesario
+            OpcionesPagoController controlOpciones = loader.getController();
+
+            vtnRegister.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
@@ -143,6 +154,14 @@ public class CatalogoController implements Initializable {
         ColumnConstraints column = new ColumnConstraints();
         ImageView imageViewPresionado = (ImageView) event.getSource();
         String imageViewId = imageViewPresionado.getId();
+        btnDel.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Botón clickeado");
+
+                tblFav.getChildren().remove(contLibSelected);
+            }
+        });
 
         if (imageViewId.equals("btnAggFav1")) {
 
@@ -186,78 +205,158 @@ public class CatalogoController implements Initializable {
 
             tblFav.getChildren().add(contLibSelected);
         }
+                mostrarAlerta("INFORMACION","PRODUCTO AGREGADO A FAVORITOS");
+
     }
 
     @FXML
     private void volverCatalogoBtn(ActionEvent event) {
+        contCar.setVisible(false);
+        contTblFav.setVisible(false);
+
     }
 
     @FXML
     private void VolverCat(MouseEvent event) {
         contTblFav.setVisible(false);
-    }
-
-    @FXML
-    private void verCarrito(ActionEvent event) {
-    }
-
-    @FXML
-    private void verFav(ActionEvent event) {
-        contTblFav.setVisible(true);
+        contCar.setVisible(false);
     }
 
     @FXML
     private void AgregarCar(MouseEvent event) {
         Label lblNombre = new Label();
+        Label lblPrecio = new Label();
+        Label lblCant = new Label();
+        Label lblPrecioT = new Label();
+        Spinner<Integer> cant = new Spinner(1, 10, 1);
         Button btnDel = new Button("ELIMINAR ELEMENTO");
+
         GridPane contLibSelected = new GridPane();
         ColumnConstraints column = new ColumnConstraints();
         ImageView imageViewPresionado = (ImageView) event.getSource();
         String imageViewId = imageViewPresionado.getId();
+        btnDel.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Botón clickeado");
+
+                tblProdCar.getChildren().remove(contLibSelected);
+            }
+        });
 
         if (imageViewId.equals("btnAggCar1")) {
 
             column.setPrefWidth(tblFav.getWidth() / 2);
 
             contLibSelected.getColumnConstraints().addAll(column);
-            contLibSelected.prefWidth(tblFav.getMaxWidth());
+            contLibSelected.prefWidth(tblProdCar.getMaxWidth());
             contLibSelected.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, null, null)));
 
             lblNombre.setText(nombreProd1.getText());
+            lblPrecio.setText(textoDosPuntos(lblPrecio1));
+            lblCant.setText(String.valueOf(cant.getValue()));
+            double cantTotal = Integer.parseInt(lblCant.getText()) * Double.parseDouble(lblPrecio.getText());
+            lblPrecioT.setText(String.valueOf(cantTotal));
 
             contLibSelected.add(lblNombre, 0, 0);
-            contLibSelected.add(btnDel, 1, 0);
+            contLibSelected.add(lblPrecio, 1, 0);
+            contLibSelected.add(lblCant, 0, 1);
+            contLibSelected.add(lblPrecioT, 1, 1);
+            contLibSelected.add(btnDel, 0, 2);
+            contLibSelected.add(cant, 1, 2);
 
-            tblFav.getChildren().add(contLibSelected);
+            tblProdCar.getChildren().add(contLibSelected);
         } else if (imageViewId.equals("btnAggCar2")) {
             column.setPrefWidth(tblFav.getWidth() / 2);
 
             contLibSelected.getColumnConstraints().addAll(column);
-            contLibSelected.prefWidth(tblFav.getMaxWidth());
+            contLibSelected.prefWidth(tblProdCar.getMaxWidth());
             contLibSelected.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, null, null)));
 
             lblNombre.setText(nombreProd2.getText());
+            lblPrecio.setText(textoDosPuntos(lblPrecio2));
+            lblCant.setText(String.valueOf(cant.getValue()));
+            double cantTotal = Integer.parseInt(lblCant.getText()) * Double.parseDouble(lblPrecio.getText());
+            lblPrecioT.setText(String.valueOf(cantTotal));
 
             contLibSelected.add(lblNombre, 0, 0);
-            contLibSelected.add(btnDel, 1, 0);
+            contLibSelected.add(lblPrecio, 1, 0);
+            contLibSelected.add(lblCant, 0, 1);
+            contLibSelected.add(lblPrecioT, 1, 1);
+            contLibSelected.add(btnDel, 0, 2);
+            contLibSelected.add(cant, 1, 2);
 
-            tblFav.getChildren().add(contLibSelected);
+            tblProdCar.getChildren().add(contLibSelected);
         } else if (imageViewId.equals("btnAggCar3")) {
 
             column.setPrefWidth(tblFav.getWidth() / 2);
 
             contLibSelected.getColumnConstraints().addAll(column);
-            contLibSelected.prefWidth(tblFav.getMaxWidth());
+            contLibSelected.prefWidth(tblProdCar.getMaxWidth());
             contLibSelected.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, null, null)));
 
             lblNombre.setText(nombreProd3.getText());
+            lblPrecio.setText(textoDosPuntos(lblPrecio3));
+            lblCant.setText(String.valueOf(cant.getValue()));
+            double cantTotal = Integer.parseInt(lblCant.getText()) * Double.parseDouble(lblPrecio.getText());
+            lblPrecioT.setText(String.valueOf(cantTotal));
 
             contLibSelected.add(lblNombre, 0, 0);
-            contLibSelected.add(btnDel, 1, 0);
+            contLibSelected.add(lblPrecio, 1, 0);
+            contLibSelected.add(lblCant, 0, 1);
+            contLibSelected.add(lblPrecioT, 1, 1);
+            contLibSelected.add(btnDel, 0, 2);
+            contLibSelected.add(cant, 1, 2);
 
-            tblFav.getChildren().add(contLibSelected);
+            tblProdCar.getChildren().add(contLibSelected);
+        }
+        mostrarAlerta("INFORMACION","PRODUCTO AGREGADO AL CARRITO");
+    }
+
+    @FXML
+    private void abrirCarrito(ActionEvent event) {
+        contCar.setVisible(true);
+
+    }
+
+    @FXML
+    private void abrirFavoritos(ActionEvent event) {
+        contTblFav.setVisible(true);
+
+    }
+
+    public String textoDosPuntos(Label label) {
+        String textoCompleto = label.getText();
+        // Buscar la posición del primer carácter dos puntos
+        int indiceDosPuntos = textoCompleto.indexOf(':');
+
+        // Verificar si se encontró el primer carácter dos puntos
+        if (indiceDosPuntos != -1 && indiceDosPuntos < textoCompleto.length() - 1) {
+            // Obtener el texto después de dos puntos
+            String textoDespuesDosPuntos = textoCompleto.substring(indiceDosPuntos + 1);
+
+            // Imprimir o utilizar el texto obtenido
+            return textoDespuesDosPuntos;
+        } else {
+            // Manejar el caso en el que no se encuentren dos puntos o estén al final del texto
+            return null;
         }
     }
 
+    public void mostrarAlerta(String titulo, String msj) {
+        // Crear una alerta de información
+        Alert alerta = new Alert(Alert.AlertType.INFORMATION);
+        alerta.initStyle(StageStyle.UTILITY);
+        alerta.setTitle(titulo);
+        alerta.setHeaderText(null);
+        alerta.setContentText(msj);
+
+        // Agregar un botón de cierre
+        ButtonType cerrarBoton = new ButtonType("Cerrar");
+        alerta.getButtonTypes().setAll(cerrarBoton);
+
+        // Mostrar la alerta y esperar a que el usuario cierre
+        alerta.showAndWait();
+    }
 
 }
